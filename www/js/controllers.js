3f0a4f080dc3da8d116cb60a8a7b6ee5c42f6099
McEileen <eileenm@gmail.com>
2016-04-25 20:57:10 -0500

    got unicorn imgs to show, need to make a stream

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,33 +2,32 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('UnicornsCtrl', function($scope, Unicorns) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  $scope.$on('$ionicView.enter', function(e) {
-  });
+.controller('UnicornsCtrl', ['$scope', 'Unicorns', function($scope, Unicorns) {
+  Unicorns.success(function(data) {
+    $scope.UnicornsStuff = data["data"];
+  })
+}])
 
-  $scope.getData = function() {
-    $http.get("http://localhost/example.json")
-      .success(function(data) {
-        $scope.type = data.type;
-        $scope.id = data.id;
-      })
-      .error(function(data) {
-        alert("ERROR");
-      });
-  }
-})
 
-.controller('UnicornDetailCtrl', function($scope, $stateParams, Unicorns) {
-  $scope.unicorn = Unicorns.get($stateParams.unicornId);
-})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+// .controller('UnicornsCtrl', function($scope, Unicorns) {
+//   // With the new view caching in Ionic, Controllers are only called
+//   // when they are recreated or on app start, instead of every page change.
+//   // To listen for when this page is active (for example, to refresh data),
+//   // listen for the $ionicView.enter event:
+//   //
+//   $scope.$on('$ionicView.enter', function(e) {
+//   });
+
+
+// })
+
+// .controller('UnicornDetailCtrl', function($scope, $stateParams, Unicorns) {
+//   $scope.unicorn = Unicorns.get($stateParams.unicornId);
+// })
+
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// });
--- www/js/services.js
+++ www/js/services.js
@@ -3,8 +3,6 @@ angular.module('starter.services', [])
 .factory('Unicorns', ['$http', function($http) {
   return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn+unicorns&api_key=dc6zaTOxFJmzC&fmt=json')
             .success(function(data) {
-              console.log("success");
-              console.log(data);
               return data;
             })
             .error(function(err) {
