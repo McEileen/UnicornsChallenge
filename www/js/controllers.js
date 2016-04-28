fd00ee99d1ba18a138938ea4ff5d71bd6957b68a
McEileen <eileenm@gmail.com>
2016-04-28 18:39:58 -0500

    add showImages, showModal & closeModal to unicorns controller, still not working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,6 +15,27 @@ angular.module('starter.controllers', [])
           $scope.$broadcast('scroll.infiniteScrollComplete');
       });
     };
+
+    $scope.showImages = function(index) {
+      $scope.activeSlide = index;
+      $scope.showModal('templates/image-popover.html');
+    };
+
+    $scope.showModal = function(templateUrl) {
+      $ionicModal.fromTemplateUrl(templateUrl, {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal;
+        $scope.modal.show();
+      });
+    }
+
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+      $scope.modal.remove()
+    };
+
 });
 
 // .controller('UnicornsCtrl', ['$scope', 'Unicorns', function($scope, Unicorns) {
