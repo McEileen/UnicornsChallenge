6bfa866ef808d21a0a194977f6f975226e714a7f
McEileen <eileenm@gmail.com>
2016-04-28 17:29:48 -0500

    clean up services

--- www/js/services.js
+++ www/js/services.js
@@ -7,14 +7,7 @@ angular.module('starter.services', [])
   return {
           GetUnicorns: function(){
                     return $http.get("http://api.giphy.com/v1/gifs/search?q=unicorn+unicorns&api_key=dc6zaTOxFJmzC&fmt=json&limit=100").then(function(response){
-                      // console.log("IN GET UNICORNS");
-                      // console.log("response below");
-                      // console.log(response);
                       unicorns = response.data.data;
-                      // console.log("should see all 100 unicorn objects below" )
-                      // console.log(unicorns);
-                      // console.log("should see url below")
-                      // console.log(unicorns[0].images.fixed_height.url);
                       return unicorns;
                     });
           },
