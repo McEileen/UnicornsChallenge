0a6659c60731f3fa7b84738453d04363bceaf2b2
McEileen <eileenm@gmail.com>
2016-04-28 21:39:59 -0500

    clean up controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,36 +38,6 @@ angular.module('starter.controllers', [])
 
 });
 
-// .controller('UnicornsCtrl', ['$scope', 'Unicorns', function($scope, Unicorns) {
-//   Unicorns.success(function(data) {
-//     $scope.UnicornsStuff = data["data"];
-//   })
-// }])
-
-
-// .controller('UnicornsCtrl', ['$scope', 'Unicorns', function($scope, Unicorns) {
-//   Unicorns.success(function(data) {
-//     $scope.UnicornsStuff = data["data"];
-//   })
-// }])
-
-
-
-// .controller('UnicornsCtrl', function($scope, Unicorns) {
-//   // With the new view caching in Ionic, Controllers are only called
-//   // when they are recreated or on app start, instead of every page change.
-//   // To listen for when this page is active (for example, to refresh data),
-//   // listen for the $ionicView.enter event:
-//   //
-//   $scope.$on('$ionicView.enter', function(e) {
-//   });
-
-
-// })
-
-// .controller('UnicornDetailCtrl', function($scope, $stateParams, Unicorns) {
-//   $scope.unicorn = Unicorns.get($stateParams.unicornId);
-// })
 
 // .controller('AccountCtrl', function($scope) {
 //   $scope.settings = {
