2053db5de434111c292fa7c2ae497438f4169e6b
McEileen <eileenm@gmail.com>
2016-05-05 10:31:48 -0500

    base results SearchServicefactory on UnicornService factory

--- www/js/services.js
+++ www/js/services.js
@@ -20,8 +20,22 @@ angular.module('starter.services', [])
   }
 })
 
-.factory('SearchService', function($rootScope) {
-  query = {};
-  // query.entry = '';
-  return query;
+.factory('SearchService', function() {
+  var API_URL = "http://api.giphy.com/v1/gifs/search?q=";
+  var API_URL_CALL_FINISH = "&api_key=dc6zaTOxFJmzC&fmt=json&limit=100";
+
+  return {
+          GetResults: function(){
+                    return $http.get(API_URL + query +API_URL_CALL_FINISH).then(function(response){
+                    results = response.data.data;
+                    return results;
+                    });
+          },
+          GetMoreResults: function(){
+                    return $http.get(API_URL + query + API_URL_CALL_FINISH).then(function(response){
+                      results = response.data.results;
+                      return results;
+                    });
+          }
+  }
 });
